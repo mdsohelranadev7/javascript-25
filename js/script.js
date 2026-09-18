ef2884f259ccf497added65759a31815386c4e30
Sohel Rana <mdsohelrana191807@gmail.com>
2026-09-18 22:18:40 +0600

    SDF

--- js/script.js
+++ js/script.js
@@ -13,47 +13,33 @@ let addbalance = document.getElementById("addbalance")
 let cashoutbalance = document.getElementById("cashoutbalance")
 let current = document.getElementById("current")
 
-
-
 loginbtn.addEventListener('click', () => {
 
     if (inputnumber.value == "1234" && inputpassword.value == "123") {
         logingpart.style.display = "none"
         cardpart.removeAttribute('hidden')
-
     }
 
     else {
         alert('type this number: 1234 or Password:123')
     }
-
 })
-
 
 addmoneybtn.addEventListener('click', () => {
     addmoneyinput.removeAttribute('hidden')
     cashoutinput.setAttribute('hidden', true)
-
-
-
-
 })
-
 
 cashoutbtn.addEventListener('click', () => {
     cashoutinput.removeAttribute('hidden')
     addmoneyinput.setAttribute('hidden', true)
 })
 
-
 addmoney.addEventListener('click', () => {
-
     if (addbalance.value == "") {
         alert('please enter your add amount')
         return
     }
-
-
 
     let kss = parseInt(current.innerText)
     let ks = parseInt(addbalance.value)
@@ -61,13 +47,13 @@ addmoney.addEventListener('click', () => {
     addbalance.value = ""
 
 })
-cashout.addEventListener('click', () => {
 
+
+cashout.addEventListener('click', () => {
     if (cashoutbalance.value == "") {
         alert('please enter your cashout amount')
         return
     }
-
 
     let kss = parseInt(current.innerText)
     let ks = parseInt(cashoutbalance.value)
